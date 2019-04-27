a2111b9e509dd4ca21b558f791a01799fae6017b
feiernanduo22 <feiernanduo22@163.com>
2019-04-27 23:16:41 +0800

    div居中

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-first
-second
-third
+
+$(window).resize(function(){ 
+    $(".mydiv").css({ 
+        'position': "absolute", 
+        'left': ($(window).width() - $(".mydiv").outerWidth())/2, 
+        'top': ($(window).height() - $(".mydiv").outerHeight())/2
+    });        
+});
+
+
+
